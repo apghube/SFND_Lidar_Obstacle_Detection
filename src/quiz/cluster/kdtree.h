82e1ae4a022321f0e56075eb8542bedc06ed4147
apghube <apghube@mtu.edu>
2020-03-29 22:42:48 -0400

    Quiz Cluster: Implemented K-D Tree

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -24,11 +24,36 @@ struct KdTree
 	KdTree()
 	: root(NULL)
 	{}
+    void insertHelper(Node* &node, uint depth, std::vector<float> point, int id)
+	{
+		if(node == NULL)
+		{
+			node=new Node(point,id);
+		}
+		else
+		{
+			/*determine the dim to be compared based on the depth, 0=x, 1=y
+			if depth is even, dim is x and if depth is odd, dim is y */
 
+			uint cd=depth%2;  
+
+			if(point[cd] < node->point[cd]) 
+			{
+                insertHelper(node->left,depth+1,point,id);
+			}
+			else
+			{
+				insertHelper(node->right,depth+1,point,id);
+			}
+			
+		}
+		
+	}
 	void insert(std::vector<float> point, int id)
 	{
 		// TODO: Fill in this function to insert a new point into the tree
 		// the function should create a new node and place correctly with in the root 
+	    insertHelper(root,0,point,id); //here depth is 0 as root is at 0 level
 
 	}
 
